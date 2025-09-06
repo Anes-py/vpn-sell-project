d9bfa931f33576c3a4895f5a560e6b190005a6ca
Anes-py <AnesDevpy@gmail.com>
2025-09-06 13:30:47 +0330

    add servers/locations context data with active filtering

--- vpnshop/views.py
+++ vpnshop/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
+from .models import Product, ServersLocations
+
 def home_page_view(request):
-    return render(request, 'vpnshop/home.html')
+    context = {
+    'products':Product.objects.filter(is_active=True),
+    'servers':ServersLocations.objects.filter(is_active=True),
+    }
+    return render(
+        request,
+        'vpnshop/home.html',
+        context=context,
+    )
