83f3e494500e0ac46ba45e04f98690ad2010635b
Anes-py <AnesDevpy@gmail.com>
2025-09-06 13:34:38 +0330

    update vpnshop models fields

--- vpnshop/models.py
+++ vpnshop/models.py
@@ -3,6 +3,8 @@ from django.utils.text import gettext_lazy as _
 
 class ServersLocations(models.Model):
     name = models.CharField(_("name"), max_length=155)
+    name2 = models.CharField(_("name2"), max_length=155)
+    region = models.CharField(_("region"), max_length=155)
     ping = models.PositiveIntegerField(_("ping"))
     server_load = models.PositiveIntegerField(_("server load"))
     is_active = models.BooleanField(_("is active"), default=True)
@@ -15,9 +17,10 @@ class ServersLocations(models.Model):
 
 class Product(models.Model):
     name = models.CharField(_("name"), max_length=155)
-    data_limit = models.PositiveIntegerField(_("data limit"), help_text=_("unlimited:0"))
-    device_connections = models.PositiveIntegerField(_("device connections"), help_text=_("unlimited:0"))
-    server_locations_count = models.PositiveIntegerField(_("server locations_count"))
+    data_limit = models.CharField(_("data limit"), help_text=_("unlimited:0"))
+    time_limit = models.CharField(_("time limit"), help_text=_("unlimited:0"))
+    device_connections = models.CharField(_("device connections"), help_text=_("unlimited:0"))
+    server_locations_count = models.CharField(_("server locations_count"))
     plan_link = models.URLField()
     plan_price = models.PositiveIntegerField(_("price"))
     is_active = models.BooleanField(_("is active"), default=True)
